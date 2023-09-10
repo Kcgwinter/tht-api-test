f988f88243780a2d3d7faee4d473707c036849c9
Kcgwinter <kcgwinter@gmail.com>
2023-09-10 19:31:31 +0200

    Update Api Routes with Comments

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
 // Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1',  'middleware' => 'auth:sanctum'], function () {
+
+    // Customer
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('addresses', AddressController::class);
+
+    // Company
     Route::apiResource('companies', CompanyController::class);
     Route::get("companies/{id}/withEmployees",'CompanyController@showWithEmployees');
     Route::apiResource('employees', EmployeeController::class);
+
+    // Requests
     Route::apiResource('requests', RequestController::class);
     Route::apiResource('appointmentDays', AppointmentDaysController::class);
     Route::apiResource('appointmentTimes', AppointmentTimesController::class);
